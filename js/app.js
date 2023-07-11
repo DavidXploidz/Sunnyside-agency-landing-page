5120dc94c698dfc8adb970fe88fa7d9ad949840f
DavidXploidz <david.adame.itd@gmail.com>
2023-07-11 09:48:34 -0600

    footer agregado

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Se crea una instancia de IntersectionObserver para iterar sobre los elementos
+// y poder agregar una clase de animación
+const observador = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrada) => {
+        // console.log(entrada);
+        // Si solo se quiere que aparezca una sola vez hacerlo solo con un if si no agregar un else
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('show');
+        }else{
+            entrada.target.classList.remove('show');
+        }
+    })
+});
+
+// Selecciona los elementos que tienen la clase especial .hidden para identicar los que se quieren animar
+const elementosOcultos = document.querySelectorAll('.hidden');
+
+// Itera sobre los elementos seleccionados y mandar llamar la funcion de observer declarada arriba
+elementosOcultos.forEach((elemento) => {
+    observador.observe(elemento)
+});
